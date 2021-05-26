7fd90964e504b927b4442fc329e5ac440729d90e
ruben-779 <ruben_1995_fcv@hotmail.com>
2021-05-26 21:00:09 +0100

    Solucionar problemas con el cors

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 app.use(express.json());
 
-var mongoose = require("mongoose");
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
+const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/appNewsletter", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
   useCreateIndex: true,
 });
-
 //router
-var routerUser = require("./api/users/users.router.js");
-var routerNews = require("./api/news/news.router.js");
-var routerTags = require("./api/tags/tags.router.js");
+const routerUser = require("./api/users/users.router.js");
+const routerNews = require("./api/news/news.router.js");
+const routerTags = require("./api/tags/tags.router.js");
 app.use("/users", routerUser);
 app.use("/news", routerNews);
 app.use("/tags", routerTags);
